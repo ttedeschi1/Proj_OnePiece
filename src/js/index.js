0803987f3335fec10fec5454ebdb97bfda9a9380
Thamyres Viana <84295234+ttedeschi1@users.noreply.github.com>
2023-09-30 22:03:03 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,25 @@
+const botoes = document.querySelectorAll('.botao');
+
+const personagens = document.querySelectorAll(".personagem");
+
+
+botoes.forEach((botao, indice) => {
+    botao.addEventListener("click", () => {
+
+        desselecionarBotao();
+
+        botao.classList.add("selecionado");
+
+        const personagemSelecionado = document.querySelector(".personagem.selecionado");
+        personagemSelecionado.classList.add("selecionado");
+        personagemSelecionado.classList.remove("selecionado");
+
+
+        personagens [indice].classList.add("selecionado");
+    })
+})
+
+function desselecionarBotao() {
+    const botaoSelecionado = document.querySelector(".botao.selecionado");
+    botaoSelecionado.classList.remove("selecionado");
+}
